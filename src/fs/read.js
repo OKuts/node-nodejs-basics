1c56b461516f472e71ff6982812cb04a8a3f1173
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:25:19 +0200

    feat: fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
-};
+import {readFile} from "node:fs/promises"
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
 
-await read();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const read = async (file) => {
+  try {
+    const content = await readFile(file)
+    console.log(content.toString())
+  } catch (e) {
+    throw new Error('FS operation failed must be thrown')
+  }
+}
+
+await read(`${__dirname}/files/fileToRead.txt`)
