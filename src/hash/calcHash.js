18259f6a5bade1925e0e5f4049294306ac295fb8
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:38:26 +0200

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import {createHash} from 'crypto'
+import {readFile} from 'node:fs/promises'
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(__dirname + '/files/fileToCalculateHashFor.txt')
+    const hash = createHash('sha256').update(content)
+    console.log(hash.digest('hex'))
+  } catch (err) {
+    throw err
+  }
 };
 
-await calculateHash();
+await calculateHash()
