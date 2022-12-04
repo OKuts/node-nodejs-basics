83b480cbe08d817fa7136e127573b329ab2eee22
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:31:09 +0200

    feat: sli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envMap = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith('RSS_')) acc.push(`${key}=${value}`)
+    return acc
+  }, [])
+  console.log(envMap.join('; '))
 };
 
 parseEnv();
