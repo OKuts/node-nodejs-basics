cd0946fd490a8c49303d51e45babff16c4a56da1
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:16:49 +0200

    feat: fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
-const rename = async () => {
-    // Write your code here 
+import {rename as rn} from 'node:fs/promises'
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const rename = async (path, name) => {
+  try {
+    const fileName = name.substr(0, name.lastIndexOf('.'))
+    await rn(`${path}/${name}`, `${path}/${fileName}.md`)
+  } catch (e) {
+    throw new Error('FS operation failed must be thrown')
+  }
 };
 
-await rename();
+await rename(`${__dirname}/files`, 'wrongFilename.txt')
