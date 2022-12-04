ed68acc3f4df8d8d6e984e89274873795f0ab62f
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:22:11 +0200

    feat: fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
-const list = async () => {
-    // Write your code here 
+import {readdir} from "node:fs/promises"
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const list = async (path) => {
+  try {
+    const files = await readdir(path, {withFileTypes: true})
+    for (const file of files) {
+      if (file.isDirectory()) {
+        await list(`${path}/${file.name}`)
+      } else {
+        console.log(file.name)
+      }
+    }
+  } catch (e) {
+    throw new Error('FS operation failed must be thrown')
+  }
 };
 
-await list();
+await list(`${__dirname}/files`)
