7ff78f208fcb75adbd24a08c4ddcfdfe65c67f28
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-04 23:08:03 +0200

    feat: fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
-const copy = async () => {
-    // Write your code here 
-};
+import {mkdir, readdir, copyFile} from 'node:fs/promises'
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
 
-await copy();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const copy = async (from, to) => {
+  try {
+    await mkdir(to)
+    const files = await readdir(from, {withFileTypes: true})
+    for (const file of files) {
+      if (file.isDirectory()) {
+        copy(`${from}/${file.name}`, `${to}/${file.name}`)
+      } else {
+        await copyFile(`${from}/${file.name}`, `${to}/${file.name}`)
+      }
+    }
+  } catch (e) {
+    throw new Error('FS operation failed must be thrown')
+  }
+}
+
+copy(__dirname + '/files', __dirname + '/filesCopy')
