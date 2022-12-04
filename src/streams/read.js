9b5cadb12d6a8364bdafd511447a5b0dfe52d3a2
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-05 00:01:26 +0200

    feat: streams/write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'fs'
+import {pipeline} from 'stream'
+import { fileURLToPath } from 'url'
+import {dirname} from 'path'
 
-await read();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const read = async (dirname, filename) => {
+  const readStream = createReadStream(`${__dirname}/${filename}`)
+
+  pipeline (
+    readStream,
+    process.stdout,
+    err => {
+      if (err) console.log(err.message)
+    }
+  )
+}
+
+await read(__dirname, 'files/fileToRead.txt')
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
-const write = async () => {
-    // Write your code here 
-};
+import {createWriteStream} from 'fs'
+import {pipeline} from 'stream'
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
 
-await write();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const write = async (dirname, filename) => {
+  const writeStream = createWriteStream(`${__dirname}/${filename}`)
+
+  pipeline(
+    process.stdin,
+    writeStream,
+    err => {
+      if (err) console.log(err.message)
+    }
+  )
+
+  console.log('For write text to file, input anything to console and press ENTER')
+}
+
+await write(__dirname, 'files/fileToWrite.txt')
