53b0db62212be784c78ecd6da7ac1c1a659e33c8
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-05 05:50:32 +0200

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import {Worker} from 'worker_threads'
+import {cpus} from 'os'
 
-await performCalculations();
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const performCalculations = async (path) => {
+  const coreArr = cpus()
+  const workerOut = await Promise.allSettled(coreArr.map((_, i) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(path, {workerData: i + 10})
+      worker.on('message', data => resolve(data))
+      worker.on('error', reject)
+    })
+  }))
+
+  const result = workerOut.map(({status, value}) => (
+    status === 'fulfilled'
+      ? {status: 'resolved', data: value}
+      : {status: 'error', data: null}
+  ))
+
+  console.log(result)
+}
+
+await performCalculations(`${__dirname}/worker.js`)
